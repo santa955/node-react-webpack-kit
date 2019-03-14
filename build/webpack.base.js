5edca6a85cf933f63970b353752c4aca1d61b994
chenzhijun <chenzhijun@hujiang.com>
2019-03-14 19:20:57 +0800

    添加webpack配置

---
+++ build/webpack.base.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack')
+const convert = require('koa-connect')
+const history = require('connect-history-api-fallback')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+const commonPaths = require('./paths')
+
+//https://imweb.io/topic/5b66dd601402769b60847149
+//https://juejin.im/post/5b5d6d6f6fb9a04fea58aabc
+const vendorPkg = [
+  'react',
+  'react-dom',
+  // 'redux',
+  // 'redux-thunk',
+  // 'react-redux',
+  // 'react-router-dom',
+  // 'react-router-redux',
+  // 'history',
+  'prop-types',
+  // 'react-loadable',
+]
+
+module.exports = {
+  entry: {
+    vendor: vendorPkg,
+    app: commonPaths.entryPath
+  },
+  module: {
+    rules: [
+      // {
+      //   enforce: 'pre',
+      //   test: /\.(js|jsx)$/,
+      //   loader: 'eslint-loader',
+      //   exclude: /(node_modules)/,
+      //   options: {
+      //     emitWarning: process.env.NODE_ENV !== 'development',
+      //   },
+      // },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.imagesFolder,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.fontsFolder,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  serve: {
+    add: app => {
+      app.use(convert(history()))
+    },
+    content: commonPaths.entryPath,
+    dev: {
+      publicPath: commonPaths.outputPath,
+    },
+    open: true,
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.css', '.scss'],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async',
+    })
+  ],
+}
---
+++ build/webpack.dev.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const commonPaths = require('./paths')
+const basecConfig = require('./webpack.base')
+
+module.exports = webpackMerge(basecConfig, {
+  mode: 'development',
+  output: {
+    filename: '[name].[hash].js',
+    path: commonPaths.outputPath,
+    chunkFilename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              camelCase: true,
+              localIdentName: '[local]___[hash:base64:5]',
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: commonPaths.outputPath,
+    compress: true,
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(['../dist']),
+    new webpack.DefinePlugin({
+      'process.env': 'development'
+    }),
+  ],
+})
